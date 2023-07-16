9222c791393d1393a1abf0f69783db797f98dbdc
Facundo Cruz <facundocruz0604@gmail.com>
2023-07-15 21:36:37 -0300

    Configuración básica del proyecto

---
+++ src/test/java/com/remax/alquileres/AlquileresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remax.alquileres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlquileresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
